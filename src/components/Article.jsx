544e65b1042f5a9b8c8dcb21c604235f27243241
jeison0894 <jeisson0894@gmail.com>
2022-03-23 19:54:43 -0500

    Article image - changed

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,6 +3,7 @@ import articleImage from "../assets/img/articleImage.jpg"
 const Article = () => {
   return (
     <article className="article">
+
       <img src={articleImage} alt="Imagen del articulo" />
 
       <p className="articleTag">Curiosidades</p>
