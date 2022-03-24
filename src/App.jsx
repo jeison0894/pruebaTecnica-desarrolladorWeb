8b7219b79a23911471d908227f310876cb35ef9b
jeison0894 <jeisson0894@gmail.com>
2022-03-23 19:39:50 -0500

    Hero section - added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from "./components/Header"
 import Articles from "./components/Articles"
 import Form from "./components/Form"
 import FAQ from "./components/FAQ"
+import Hero from "./components/Hero"
 
 const App = () => {
   return (
     <>
       <Header />
       <main>
+        <Hero />
         <Articles />
         <Form />
         <FAQ />
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+
+const Hero = () => {
+  return (
+    <div className="slider">
+      <div className="wrapper">
+        <img src="https://images.unsplash.com/photo-1609220136736-443140cffec6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Familia" />
+        <img src="https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Amigos" />
+        <img src="https://images.unsplash.com/photo-1601512310580-da458161fcab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Amor" />
+        <img src="https://images.unsplash.com/photo-1524503033411-c9566986fc8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Risas" />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
+
