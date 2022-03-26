b51e9bc14da5f3e07f7952c6f681600d7ae8dfa3
jeison0894 <jeisson0894@gmail.com>
2022-03-26 10:57:12 -0500

    Newsletter illustration - added

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -2,12 +2,12 @@
 const FAQ = () => {
   return (
     <section className="FAQ">
-      <details className="FAQ__details">
+      <details className="FAQ__details" open>
         <summary className="FAQ__summary">¿Cómo tomar mejores decisiones?</summary>
         <p className="FAQ__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum, fusce donec laoreet quam volutpat pellentesque aenean lacus vulputate. Scelerisque fermentum vivamus vel est consectetur.</p>
       </details>
 
-      <details className="FAQ__details" open>
+      <details className="FAQ__details" >
         <summary className="FAQ__summary">¿Cómo tomar mejores decisiones?</summary>
         <p className="FAQ__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum, fusce donec laoreet quam volutpat pellentesque aenean lacus vulputate. Scelerisque fermentum vivamus vel est consectetur.</p>
       </details>
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,15 +1,21 @@
 import { useContext } from "react"
 import AppContext from "../context/appContext"
+import ilustration from "../assets/img/ilustration.svg"
 
 const Newsletter = () => {
   const { handleModal } = useContext(AppContext)
 
   return (
     <section className="newsletter">
-      <div className="newsletter__container">
-        <h2>Suscríbete a nuestro newsletter</h2>
-        <p className="newsletter__text">Mantente al tanto del lanzamiento de nuevos artículos.</p>
-        <button className="newsletter__button" onClick={() => handleModal(true)}>Suscribirse a nuestro newsletter</button>
+      <div className="newsletter__wrapper">
+        <div className="newsletter__container">
+          <img className="newsletter__ilustration" src={ilustration} alt="" />
+          <div>
+            <h2 className="newsletter__title">Suscríbete a nuestro newsletter</h2>
+            <p className="newsletter__text">Mantente al tanto del lanzamiento de nuevos artículos.</p>
+            <button className="newsletter__button" onClick={() => handleModal(true)}>Suscribirse</button>
+          </div>
+        </div>
       </div>
     </section>
   );
